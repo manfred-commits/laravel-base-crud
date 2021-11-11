5d40b3c2df9bef9587f39a6b28e728b66cacc49f
manfred-commits <manfredipiraino@gmail.com>
2021-11-11 18:01:20 +0100

    added ternary operator for old method in edit

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -10,14 +10,14 @@
 
 	<div class="form-group">
         <label for="title">Titolo</label>
-		<input type="text" class="form-control" id="title" name="title" placeholder="Insersci titolo" value="{{$comic['title']}}">
+		<input type="text" class="form-control" id="title" name="title" placeholder="Insersci titolo" value="{{old('title') ? old('title') : $comic['title']}}">
         @error('title')
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
     </div>
     <div class="form-group">
         <label for="description">Descrizione Comic</label>
-        <textarea class="form-control" name="description" id="description" cols="30" rows="10" placeholder="Insersci la descrizione">{{$comic['description']}}</textarea>
+        <textarea class="form-control" name="description" id="description" cols="30" rows="10" placeholder="Insersci la descrizione">{{old('description') ? old('description') : $comic['description']}}</textarea>
         @error('description')
 			<div class="alert alert-danger">{{ $message }}</div>
 		@enderror
@@ -25,28 +25,28 @@
 
 	<div class="form-group">
 		<label for="thumb">link img</label>
-		<input type="text" class="form-control" id="thumb" name="thumb" placeholder="Insersci il link img" value="{{$comic['thumb']}}">
+		<input type="text" class="form-control" id="thumb" name="thumb" placeholder="Insersci il link img" value="{{old('thumb') ? old('thumb') : $comic['thumb']}}">
         @error('thumb')
 			<div class="alert alert-danger">{{ $message }}</div>
 		@enderror
     </div>
     <div class="form-group">
         <label for="price">Prezzo</label>
-        <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Insersci il prezzo" value="{{$comic['price']}}">
+        <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Insersci il prezzo" value="{{old('price') ? old('price') : $comic['price']}}">
         @error('price')
 			<div class="alert alert-danger">{{ $message }}</div>
 		@enderror
     </div>
     <div class="form-group">
 		<label for="series">serie</label>
-		<input type="text" class="form-control" id="series" name="series" placeholder="Insersci tipo di serie" value="{{$comic['series']}}">
+		<input type="text" class="form-control" id="series" name="series" placeholder="Insersci tipo di serie" value="{{old('series') ? old('series') : $comic['series']}}">
         @error('series')
 			<div class="alert alert-danger">{{ $message }}</div>
 		@enderror
     </div>
 	<div class="form-group">
 		<label for="sale_date">Data di rilascio</label>
-		<input value="{{$comic['sale_date']}}" type="date" class="form-control" id="sale_date" name="sale_date" placeholder="Insersci la data di rilascio">
+		<input value="{{old('sale_date') ? old('sale_date') : $comic['sale_date']}}" type="date" class="form-control" id="sale_date" name="sale_date" placeholder="Insersci la data di rilascio">
         @error('sale_date')
 			<div class="alert alert-danger">{{ $message }}</div>
 		@enderror
@@ -54,7 +54,7 @@
 
 	<div class="form-group">
 		<label for="type">tipo</label>
-		<input type="text" class="form-control" id="type" name="type" placeholder="Insersci il tipo di comic" value="{{$comic['type']}}">
+		<input type="text" class="form-control" id="type" name="type" placeholder="Insersci il tipo di comic" value="{{old('type') ? old('type') : $comic['type']}}">
         @error('type')
 			<div class="alert alert-danger">{{ $message }}</div>
 		@enderror
