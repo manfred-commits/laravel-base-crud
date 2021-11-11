95f59ad39c80d2da8fc1896db2414f19307badd5
manfred-commits <manfredipiraino@gmail.com>
2021-11-11 17:28:25 +0100

    added button to edit routed to comics.edit

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -26,7 +26,10 @@
 		<td>{{$comic["series"]}}</td>
 		<td>{{$comic["sale_date"]}}</td>
 		<td>{{$comic["type"]}} </td>
-		<td><a href="{{route("comics.show", $comic["id"])}}"><button type="button" class="btn btn-primary">Visualizza</button></a></td>
+		<td>
+			<a href="{{route("comics.show", $comic["id"])}}"><button type="button" class="btn btn-primary">Visualizza</button></a>
+			<a href="{{route("comics.edit", $comic["id"])}}"><button type="button" class="btn btn-warning">Modifica</button></a>
+		</td>
 	  </tr>
 	  @endforeach
 	</tbody>
