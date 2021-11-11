1c01cdf578db093b7c61aa19a498cd0bfa183003
manfred-commits <manfredipiraino@gmail.com>
2021-11-11 17:56:50 +0100

    added errors to the form

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -11,35 +11,54 @@
 	<div class="form-group">
         <label for="title">Titolo</label>
 		<input type="text" class="form-control" id="title" name="title" placeholder="Insersci titolo" value="{{$comic['title']}}">
-	</div>
+        @error('title')
+            <div class="alert alert-danger">{{ $message }}</div>
+        @enderror
+    </div>
     <div class="form-group">
         <label for="description">Descrizione Comic</label>
         <textarea class="form-control" name="description" id="description" cols="30" rows="10" placeholder="Insersci la descrizione">{{$comic['description']}}</textarea>
+        @error('description')
+			<div class="alert alert-danger">{{ $message }}</div>
+		@enderror
     </div>
 
 	<div class="form-group">
 		<label for="thumb">link img</label>
 		<input type="text" class="form-control" id="thumb" name="thumb" placeholder="Insersci il link img" value="{{$comic['thumb']}}">
-	</div>
+        @error('thumb')
+			<div class="alert alert-danger">{{ $message }}</div>
+		@enderror
+    </div>
     <div class="form-group">
         <label for="price">Prezzo</label>
         <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Insersci il prezzo" value="{{$comic['price']}}">
+        @error('price')
+			<div class="alert alert-danger">{{ $message }}</div>
+		@enderror
     </div>
     <div class="form-group">
 		<label for="series">serie</label>
 		<input type="text" class="form-control" id="series" name="series" placeholder="Insersci tipo di serie" value="{{$comic['series']}}">
-	</div>
+        @error('series')
+			<div class="alert alert-danger">{{ $message }}</div>
+		@enderror
+    </div>
 	<div class="form-group">
 		<label for="sale_date">Data di rilascio</label>
 		<input value="{{$comic['sale_date']}}" type="date" class="form-control" id="sale_date" name="sale_date" placeholder="Insersci la data di rilascio">
-	</div>
-
-
+        @error('sale_date')
+			<div class="alert alert-danger">{{ $message }}</div>
+		@enderror
+    </div>
 
 	<div class="form-group">
 		<label for="type">tipo</label>
 		<input type="text" class="form-control" id="type" name="type" placeholder="Insersci il tipo di comic" value="{{$comic['type']}}">
-	</div>
+        @error('type')
+			<div class="alert alert-danger">{{ $message }}</div>
+		@enderror
+    </div>
 
 	<button type="submit" class="btn btn-primary">Modifica</button>
   </form>
