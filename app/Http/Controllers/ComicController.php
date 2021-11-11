d6fc63ace9377272472818bdcb91d1b2f66742f0
manfred-commits <manfredipiraino@gmail.com>
2021-11-11 16:52:32 +0100

    added dipendency injection in show

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -57,9 +57,10 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comic $comic)
     {   
-        $comic= Comic::find($id);
+        // $comic= Comic::find($id);
+        // dependency injection
         return view("comics.show", compact('comic'));
 
     }
